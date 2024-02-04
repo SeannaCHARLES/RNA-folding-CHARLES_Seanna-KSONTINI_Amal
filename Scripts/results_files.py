e41bf415c2081d59e9c2f36ef3e4bca6c2389fff
NaNa . <charles.seanna@outlook.fr>
2024-02-04 16:44:20 +0100

    Training script

---
+++ Scripts/results_files.py
@@ -0,0 +1,85 @@
+import csv
+import os
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+from PyPDF2 import PdfMerger
+import matplotlib.pyplot as plt
+import math
+# This file contains all the script that create a results files (csv or pdf)
+
+def creation_csv(score_dict,type):
+     with open(f'Final_res_{type}.csv', 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+
+        entete = ['Nucleotide pairs'] + [str(i) + 'A' for i in range(21)]
+        csvwriter.writerow(entete)
+
+        for couple_nu, valeurs in score_dict.items():
+            if type=="pred" :
+                ligne=[couple_nu] + [valeurs.get(i,0) for i in range(21)]
+            else : 
+                ligne=[couple_nu] + [valeurs.get(i, [0, 0, 0])[2] for i in range(21)]
+            csvwriter.writerow(ligne)
+
+def creation_score_file(score_dict):
+    for key, values in score_dict.items():
+        with open(f"Result_file_pairs_{key}.pdf", 'wb') as res:
+            pdf_canvas = canvas.Canvas(res)
+            title = f'For the pairs of nucleotide : {key}'
+            pdf_canvas.drawString(100, pdf_canvas._pagesize[1] - 50, title)
+
+            y_position = pdf_canvas._pagesize[1] - 70  
+
+            for key2, values in values.items():
+                if y_position-90 < 30:
+                    pdf_canvas.showPage()  # Passer à une nouvelle page
+                    y_position = pdf_canvas._pagesize[1] - 30  # Réinitialiser la position y
+
+                pdf_canvas.drawString(100, y_position, f'\t for the distance of : {key2} :')
+                y_position -= 20  
+                pdf_canvas.drawString(100, y_position, f'\t \t The observed probability = {values[0]}')
+                y_position -= 20  
+                pdf_canvas.drawString(100, y_position, f'\t \t The reference frequency = {values[1]}')
+                y_position -= 20  
+                pdf_canvas.drawString(100, y_position, f'\t \t The score = {values[2]}')
+                y_position -= 30  
+            pdf_canvas.save()
+        merger = PdfMerger()
+ 
+
+        pdfs = [f'results/histogram_Distribution_{key}.pdf', f'results/Log_ratio_{key}.pdf', f'results/Result_file_pairs_{key}.pdf']
+ 
+        for pdf in pdfs:
+            merger.append(pdf)
+        merger.write(f"{key}.pdf")
+        merger.close()
+        for pdf in pdfs : 
+            os.remove(pdf)
+
+def Grap_log_ratio(dict_NN_dist,counts) :
+    for key, values in dict_NN_dist.items():
+        plt.figure(figsize=(8, 4))
+        log_ratios = [math.log(value / counts[key]) for value in values]
+        plt.hist(log_ratios, bins='auto', alpha=0.7)
+        plt.xlabel(f'Log ratio log(distance/nb of apparition {key})')
+        plt.ylabel('Frequency')
+        plt.title(f'Log ratio distribution for {key}')
+        plt.savefig(f'Log_ratio_{key}.pdf') 
+        plt.close()
+def Grap_distribution(dict_NN_dist) :
+    for key, values in dict_NN_dist.items():
+        plt.figure(figsize=(8, 4))
+        plt.hist(values, bins='auto', alpha=0.7)
+        plt.xlabel('Distance values')
+        plt.ylabel('Frequency')
+        plt.title(f'Ditribution histogram for {key}')
+        plt.savefig(f'histogram_Distribution_{key}.pdf')  
+        plt.close()
+
+def Graph_pairs_distribution(counts):
+    plt.bar(counts.keys(), counts.values())
+    plt.xlabel('Nucleotide couple')
+    plt.ylabel('Number of apparitions')
+    plt.title('Number of time a nucleotide couple appears in the sequence')
+    plt.savefig(f'Number_of_time_a_nucleotide_couple_appears_in_the_sequence_chainA.pdf')  
+    plt.close()
